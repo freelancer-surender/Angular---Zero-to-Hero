ca95a5dadfc091d068b5463f3313694956b98e35
freelancer-surender <freelancer.surender@gmail.com>
2021-08-28 22:20:00 +0530

    reactive form added

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { ReactiveComponent } from './reactive/reactive.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-forms/src/app/reactive/reactive.component.ts
+++ angular-forms/src/app/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
+  myForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.myForm = new FormGroup(
+      {
+        uname: new FormControl('Surender', Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        readTerms: new FormControl(false)
+      }
+    );
+  }
+
+  onSubmit() {
+    this.myForm.reset();
+  }
+
+  accept() {
+    this.myForm.setValue({
+      readTerms: true
+    });
   }
 
 }
